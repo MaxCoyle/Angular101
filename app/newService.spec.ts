b50ed6f041e38b6b8653961d7707a4172cdb4e02
Max Coyle <max.coyle@kalibrate.com>
2019-04-04 17:35:45 +0100

    Move test around ready for testing promise

--- app/newService.spec.ts
+++ app/newService.spec.ts
@@ -1,18 +1,40 @@
 describe('New Service Factory', function() {
-    var newService: any;  
-    
-    beforeEach(angular.mock.module('angular101'));
-      
-    beforeEach(inject(function(_newService_) {
-        newService = _newService_;
-    }));  
-    
-    it('message should match', function() {
+    var newService: angular101.INewService;
 
-      var message: angular101.Message =
-          new angular101.Message("This message, built with a constructor, has come from the new service");
-      var expected = message.text;
-      var actual = newService.getMessage();
-      expect(expected).toEqual(actual);
+    describe('standard tests', function() {
+
+        beforeEach(angular.mock.module('angular101'));
+
+        beforeEach(inject(function(_newService_: angular101.INewService) {
+            newService = _newService_;
+        }));
+
+        it('message should match', function() {
+            var message: angular101.Message =
+                new angular101.Message("This message, built with a constructor, has come from the new service!");
+            var expected: string = message.text;
+            var actual: string = newService.getMessage();
+            expect(expected).toEqual(actual);
+        });
+
     });
+
+    // describe('tests with promises', function() {
+        
+    //     beforeEach(angular.mock.module('angular101'));
+          
+    //     beforeEach(function () {
+    //         newService = jasmine.createSpyObj('newService', ['getMessageViaPromise']);            
+    //     });
+
+        
+    //     it('message should match', function() {
+    //         var message: angular101.Message =
+    //             new angular101.Message("This message, built with a constructor, has come from the new service!");
+    //         var expected: string = message.text;
+    //         var actual: string = newService.getMessage();
+    //         expect(expected).toEqual(actual);
+    //     });
+
+    // });
 });
